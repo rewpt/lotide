cf2a5ccb57230675793f8c3e8bc36d7a9e1eb62c
rewpt <zpermack@gmail.com>
2021-07-21 00:32:52 +0000

    Completed head function grabbing first element of an array

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const head = (arr) => {
+  let firstItem = arr[0];
+  return firstItem
+  }
+  
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);

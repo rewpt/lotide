414184feeb1a5f00b93bf64a5bdf27c1650e8171
rewpt <zpermack@gmail.com>
2021-07-22 21:54:47 +0000

    Created to functions. eqArrays.js returns true or false if they depending on equality. assertArraysEqual.js console.logs a pretty message whether it is true or false

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1 === arr2) { return true };
+  if (arr1 === null || arr2 === null) { return false };
+  if (arr1.length !== arr2.length) { return false };
+
+/*Since we have already tested if arrays are the same length 
+then we only need to loop through one array and check it against the second*/
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) { return false };
+  }
+  return true;
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertArraysEqual([3,2,4,5], [3,2,4,5]);
+assertArraysEqual([23,5,6], [33,6,25]);
---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1 === arr2) { return true };
+  if (arr1 === null || arr2 === null) { return false };
+  if (arr1.length !== arr2.length) { return false };
+
+/*Since we have already tested if arrays are the same length 
+then we only need to loop through one array and check it against the second*/
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) { return false };
+  }
+  return true;
+}
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
